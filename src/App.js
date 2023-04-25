83bbf22fca3bbb53e8bab66521ca6a2c29238590
hudaR12 <huda.rauf@xloopdigital.com>
2023-04-25 18:03:45 +0500

    UI done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import PokemonApi from './components/PokemonApi';
+import bg from '../src/components/bg.jpg'
 function App() {
   return (
-    <>
+    <div className='container' style={{ backgroundImage:`url(${bg})` }}>
+    <h1 style={{textAlign:"center", fontFamily:"cursive",color:"white"}}>Pokemon Card</h1>
     <PokemonApi/>
-    </>
+    </div>
   );
 }
 
--- src/components/PokemonApi.jsx
+++ src/components/PokemonApi.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import left from '../components/left.png';
+import right from '../components/right.png';
+
 function PokemonApi() {
   const [data, setData] = useState([]);
   const [forward, setForward] = useState();
   const [back, setBack] = useState();
   const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
   const [pokemon, setPokemon] = useState([]);
+
   // FetchData fetch the data from the PokemonApi....
   const fetchData = (url) => {
     fetch(url)
@@ -37,43 +41,56 @@ function PokemonApi() {
           const id = pokemon.url.split('/')[6];
           const pok_Url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
           return (
-            <div
+            <div className="card mb-3 justify-content-center"
               key={id}
-              style={{ marginLeft: '40px', fontSize: '18px' }}
+              style={{ marginTop: "20px",marginLeft: '40px', fontSize: '18px',maxWidth: "540px" ,backgroundColor:"#f8ff36"}}
               onClick={() => {
                 handleClick(pok_Url);
               }}
             >
-              <h2>{pokemon.name} </h2>
+              <div className="row g-0">
+              <div class="col-md-4">
+              <h2 style={{color:"black"}}>{pokemon.name} </h2>
+              </div>
+              <div class="col-md-8">
+              <div class="card-body">
               <img
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
                 alt=""
               />
+              </div>
+              </div>
+              </div>
             </div>
           );
         })}
-      <div>
+      <div style={{textAlign:"center",  paddingTop: "40px",marginBottom:"10px"  }}>
         {back && (
-          <button onClick={() => {setUrl(back);}}>
-            Back
+          <button onClick={() => {setUrl(back);}} style={{backgroundColor:"black"}}>
+            <img src={left} style={{backgroundColor:"black", width:"50px"}}></img>
           </button>
         )}
         {forward && (
-          <button onClick={() => { setUrl(forward);}}>
-            Forward
+          <button onClick={() => { setUrl(forward);}} style={{backgroundColor:"black"}}>
+          <img src={right} style={{backgroundColor:"black", width:"50px"}}/>
           </button>
         )}
       </div>
-      {/* This is for showing the info of specific pokemon when we click the specific pokemon */}
+
+      {/* This is for displaying the info of specific pokemon when we click the specific pokemon */}
       {pokemon.species && (
-        <div>
-          <h2>{pokemon.species}</h2>
-          <img src={pokemon.img} alt="" />
-          <p>HP: {pokemon.hp}</p>
-          <p>Attack: {pokemon.attack}</p>
-          <p>Defense: {pokemon.defense}</p>
-          <p>Type: {pokemon.type}</p>
-        </div>
+      <div className="col d-flex justify-content-center">
+      <div className="card" style={{width: "18rem",marginTop:"10px",marginBottom:"50px"}}>
+      <img className="card-img-top" src={pokemon.img} alt="pokemon character"/>
+      <div class="card-body" style={{backgroundColor:"#eded68"}}>
+      <h5 class="card-title" style={{fontSize:"30px",textAlign:"center"}}>{pokemon.species}</h5>
+      <p style={{fontSize:"20px"}}><b>HP:</b> {pokemon.hp}</p>
+      <p  style={{fontSize:"20px"}}><b>Attack:</b> {pokemon.attack}</p>
+      <p  style={{fontSize:"20px"}}><b>Defense: </b>{pokemon.defense}</p>
+      <p  style={{fontSize:"20px"}}><b>Type:</b> {pokemon.type}</p>
+    </div>
+    </div>
+    </div>
       )}
     </div>
   );
